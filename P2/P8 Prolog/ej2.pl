d530232648bbc3af6271dacd36d00663a7413370
Tommy1908 <tommy.villafane3@gmail.com>
2025-06-11 00:52:25 -0300

    ej2

--- P2/P8 Prolog/ej2.pl
+++ P2/P8 Prolog/ej2.pl
@@ -1,9 +1,20 @@
 %Sea el siguiente programa lógico: vecino(X, Y, [X|[Y|Ls]]).
 %vecino(X, Y, [W|Ls]) :- vecino(X, Y, Ls).
 %i. Mostrar el árbol de búsqueda en Prolog para resolver vecino(5, Y, [5,6,5,3]), devolviendo todos los valores de Y que hacen que la meta se deduzca lógicamente del programa.
-%ii. Si se invierte el orden de las reglas, ¾los resultados son los mismos? ¾Y el orden de los resultados?
+%ii. Si se invierte el orden de las reglas, los resultados son los mismos? Y el orden de los resultados?
 
 vecino(X, Y, [X|[Y|Ls]]).
 vecino(X, Y, [W|Ls]) :- vecino(X, Y, Ls).
 
 % vecino(5, Y, [5,6,5,3])
+%  |-> vecino(5, 6, [5|[6|5,3]]) ... {Y:=6} OK
+%  |-> vecino(5, Y, [6,5,3])     ... {LS:=[6,5,3]}
+%          |-> No unifica con R1 (5/=6)
+%          |-> vecino(5, Y, [5,3]) ... {LS:=[5,3]}
+%                 |-> vecino(5, 3, [5|[3|[]]]) ... {Y:=3} OK
+
+vecino+(X, Y, [W|Ls]) :- vecino(X, Y, Ls).
+vecino+(X, Y, [X|[Y|Ls]]).
+
+%En este caso parece ser los mismos resultados, pero orden invertido
+%Entiendo que es lo mismo porque ve el caso en profundidad y luego el otro, pero con la misma instancia... a chequear
