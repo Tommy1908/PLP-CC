8dea7ae0a396d60afbfc7cfb4747f8ba1145f4a7
Tommy1908 <tommy.villafane3@gmail.com>
2025-08-08 10:55:33 -0300

    ej8

--- P2/P8 Prolog/ej8.pl
+++ P2/P8 Prolog/ej8.pl
@@ -1,4 +1,4 @@
-%Definir el predicado parteQueSuma(+L,+S,-P) que es verdadero cuando P es una lista con elementos de L que suman S. 
+%Definir el predicado parteQueSuma(+L,+S,-P) que es verdadero cuando P es una lista con elementos pertenecientes a L que suman S. 
 
 parteQueSuma([],0,[]). %si no es lista vacia se generan duplicados
 parteQueSuma([L|LX],S,[L|PS]):- S1 is S - L, S1 >= 0,parteQueSuma(LX,S1,PS).
